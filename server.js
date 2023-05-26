f7db0f05e7836d9f0ce5fb3ba1299720b8daa778
bigriver <volodsher@gmail.com>
2023-05-25 23:27:57 -0400

    mysql 1

--- server.js
+++ server.js
@@ -1,10 +1,47 @@
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const mysql = require('mysql');
 
 const app = express();
 app.use(cors());
 connectDB();
+
+const con = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+  database: process.env.DB,
+});
+
+// to work with DB
+
+con.connect(function (err) {
+  if (err) throw err;
+  console.log('Connected!');
+
+  const sql = 'INSERT INTO customers (name, adress) values ("Iryna", "Ottawa")';
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log('Added  ');
+  });
+});
+
+// con.connect(function (err) {
+//   if (err) throw err;
+//   console.log('Connected!');
+
+//   const sql =
+//     'CREATE TABLE customers3 (name VARCHAR(255), adress VARCHAR(255))';
+//   con.query(sql, function (err, result) {
+//     if (err) throw err;
+//     console.log('Table created');
+//   });
+// });
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
